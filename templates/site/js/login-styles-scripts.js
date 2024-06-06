4643d5af15d55e37479d93822a3611b9077983ee
alexandercyu <alexanderc.yu@utoronto.ca>
2024-06-06 14:03:45 -0400

    Rename initialize-tooltips.js to login-styles-scripts.js

---
+++ templates/site/js/login-styles-scripts.js
@@ -0,0 +1,39 @@
+/*Initialize Popper.js and Tooltips.js*/
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+
+})
+
+/*Toggle eye icon and password text in password input field*/
+function togglePassword() {
+    var passwordInput = document.getElementById("userPassword");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
+
+function toggleEyeIcon(element) {
+    var eyeIcon = element.firstElementChild;
+    eyeIcon.classList.toggle( "fa-eye");
+}
+
+
+window.onload = () => {
+    var userPassword = document.getElementById("userPassword");
+    var eyeIconContainer = userPassword.nextElementSibling;
+
+    eyeIconContainer.addEventListener("click", (event) =>{
+      toggleEyeIcon(eyeIconContainer);
+      togglePassword();
+    })
+}
+
+/*Show spinner on button on click*/
+function showSpinner(){
+    var animatedSpinner = document.getElementById("spinnerAnimated");
+    animatedSpinner.classList.toggle("visible")
+}
