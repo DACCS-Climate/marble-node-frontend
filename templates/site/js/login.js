27edcc50ebb5c97c3066cae7ba28a405e1157228
alexandercyu <alexanderc.yu@utoronto.ca>
2024-07-10 17:02:42 -0400

    Update login.js
    Move modal code to its own file

--- templates/site/js/login.js
+++ templates/site/js/login.js
@@ -10,36 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
     //Set contact email for node admin on the error message when login is incorrect.  Allows user to contact node
     //admin to create account
     setNodeAdminEmail();
-
-
-    // Get the modal
-var modal = document.getElementById("myModalCustom");
-
-// Get the button that opens the modal
-var btn = document.getElementById("buttonSelectProvider");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
 })
 
 function login(){
