3206796eb33bb233ea3cd164f0d6cc562a934087
alexandercyu <alexanderc.yu@utoronto.ca>
2024-07-10 16:59:35 -0400

    Create modal.js
    Script to display content in dialog tags as modal

---
+++ templates/site/js/modal.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the modal
+    var modal = document.getElementById("modalDisplay");
+    var openModalButton = document.getElementById("openModal");
+    var closeModalButton = document.getElementById("closeModal");
+
+    // When the user clicks on the button, open the modal
+    openModalButton.addEventListener("click", function(){
+         modal.showModal();
+         customizeOpenModalButton();
+    });
+    
+    closeModalButton.addEventListener("click", () => {
+        modal.close();
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.close();
+      }
+    }
+    customModalFunctions();
+})
+
