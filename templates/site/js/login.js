5013a0a10dddfc5a2c8a18ec8792cc9eca477643
alexandercyu <alexanderc.yu@utoronto.ca>
2024-07-11 17:02:42 -0400

    Update login.js
    - Add functions to swap Provider and Email sign in buttons along with their respective background images, and swapping visibility on the respective input elements
    - Add fetch request for signing in a user using a provider

--- templates/site/js/login.js
+++ templates/site/js/login.js
@@ -3,8 +3,14 @@ const nodeURL = "https://redoak.cs.toronto.edu/";
 
 document.addEventListener("DOMContentLoaded", function () {
     var signInButton =  document.getElementById("buttonSignIn");
+    var emailButton = document.getElementById("buttonSelectEmail");
+
     signInButton.addEventListener('click', (event) => {
         login();
+    })
+
+    emailButton.addEventListener('click', (event) => {
+        swapEmailProviderPanel();
     })
 
     //Set contact email for node admin on the error message when login is incorrect.  Allows user to contact node
@@ -12,44 +18,90 @@ document.addEventListener("DOMContentLoaded", function () {
     setNodeAdminEmail();
 })
 
+//Swap Select Provider and Sign In With Email buttons
+//Swap background images for right side for Provider and Email
+//Swap Provider image and name area
+//Make password input field visible again
+function swapEmailProviderPanel(){
+    var divProviderTitle = document.getElementById("divProviderTitle");
+    var divPasswordInput = document.getElementById("divPasswordInput");
+    var hiddenProviderName = document.getElementById("hiddenProviderName");
+    var divLoginContentRight = document.getElementById("loginContentRight");
+    var emailButton = document.getElementById("divEmailButton");
+    var providerButton = document.getElementById("divProviderButton");
+
+
+    divPasswordInput.classList.toggle("div-login-password-input-invisible");
+    divProviderTitle.classList.toggle("div-provider-title-visible");
+    hiddenProviderName.value = ""; //Erase provider name stored in the hidden text field
+    providerButton.classList.replace("div-provider-button-invisible", "div-provider-button");
+    emailButton.classList.replace("div-email-button", "div-email-button-invisible");
+    divLoginContentRight.classList.replace("login-email-background", "login-provider-background");
+}
+
+
 function login(){
     const usernameInput = document.getElementById("userName").value;
     const passwordInput = document.getElementById("userPassword").value;
+    const providerInput = document.getElementById("hiddenProviderName").value
 
     const nodeSignIn = nodeURL + "magpie/signin";
-    const magpieHome = nodeURL + "magpie";
+    const accountHome = "account.html";
     let loginErrorMessage = document.getElementById("loginError");
 
     //Replace nodeSignIn in fetch with endpoint when done
     //"/magpie/signin"
-    fetch(nodeSignIn, {
-        method: "POST",
-        headers: {
-            Accept: "application/json, text/plain",
-            "Content-Type": "application/json",
-            },
-        body: JSON.stringify({
-            user_name: usernameInput,
-            password: passwordInput
-            })
-        }).then(response => response.json()).then(json  => {
+    if(providerInput){
+        const providerSignIn = nodeURL + "magpie/providers/" + providerInput + "/signin";
 
-        try{
-            if(json.code == 200){
-            //Replace magpieHome with /magpie when done
-            window.location.href = magpieHome;
-        }
+        fetch(providerSignIn, {
+            method: "GET",
+            headers:{
+                 Accept: "application/json, text/plain",
+                "Content-Type": "application/json"
+            }
+        }).then(response => response.json().then(json => {
+            try{
+                if(json.code == 200){
+                    //window.location.href = accountHome;
+                    console.log(json);
+                }
 
-        }catch (error){
-             throw new Error(error)
-            loginErrorMessage.classList.toggle("login-error-visible");
-        }
+            }catch (error){
+                 throw new Error(error)
+                loginErrorMessage.classList.toggle("login-error-visible");
+            }
 
+        }))
+    }
+    else{
+        fetch(nodeSignIn, {
+            method: "POST",
+            headers: {
+                Accept: "application/json, text/plain",
+                "Content-Type": "application/json"
+                },
+            body: JSON.stringify({
+                user_name: usernameInput,
+                password: passwordInput
+                })
+            }).then(response => response.json()).then(json  => {
 
+            try{
+                if(json.code == 200){
+                    //Replace magpieHome with /magpie when done
+                    window.location.href = magpieHome;
+                }
 
-    })
+            }catch (error){
+                 throw new Error(error)
+                loginErrorMessage.classList.toggle("login-error-visible");
+            }
+        })
+    }
 }
 
+//Sets the email contact on the error message for the particular node's admin/contact
 function setNodeAdminEmail(){
     const githubURL = "{{ node_registry_url }}";
     let currentURL = window.location.href;
@@ -62,18 +114,15 @@ function setNodeAdminEmail(){
         node_keys.forEach((key, index) => {
             if(key.links){
                 key.links.forEach((link) =>{
-                if(link.rel && link.rel === "service"){
-                    if(link.href in currentURL){
-                        console.log(link.href);
-                        contactEmail.setAttribute("href", "mailto:" + link.href);
+                    if(link.rel && link.rel === "service"){
+                        if(link.href in currentURL){
+                            console.log(link.href);
+                            contactEmail.setAttribute("href", "mailto:" + link.href);
+                        }
+
                     }
-
-                }
-            })
+                })
             }
-
-
-
         })
 
     })
