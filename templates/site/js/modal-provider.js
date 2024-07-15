c57998ed64ff27c2329fa31fd54310ecc67b1065
alexandercyu <alexanderc.yu@utoronto.ca>
2024-07-15 17:03:06 -0400

    Update modal-provider.js
    Change modal button IDs

--- templates/site/js/modal-provider.js
+++ templates/site/js/modal-provider.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    setModal("providerModal", "openModal", "openModalLink", "closeModal");
+    setModal("providerModal", "openProviderModal", "openProviderModalLink", "closeProviderModal");
     setProviderButtonActions();
 })
 
