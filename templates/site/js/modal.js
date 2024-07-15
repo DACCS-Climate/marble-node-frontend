12e77c32ac4ef3566cfaff204cf29caec6b8e12b
alexandercyu <alexanderc.yu@utoronto.ca>
2024-07-15 16:27:17 -0400

    Update modal.js
    Make the modal.js more general and can be able to be customized.
    For example, the providers-modal.js can set custom modal elements for Providers.
    
    Also allows multiple modal elements to be set on one page.

--- templates/site/js/modal.js
+++ templates/site/js/modal.js
@@ -1,26 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
+function setModal(modalDialogID, openModalButtonElementID, openModalLinkElementID, closeModalElementID){
     // Get the modal
-    var modal = document.getElementById("modalDisplay");
-    var openModalButton = document.getElementById("openModal");
-    var closeModalButton = document.getElementById("closeModal");
+    var modal = document.getElementById(modalDialogID);
+    var bodyHTMLCollection = document.getElementsByTagName("body");
+    var bodyTags = Array.from(bodyHTMLCollection);
+    var openModalButton = document.getElementById(openModalButtonElementID);
+    var openModalLink = document.getElementById(openModalLinkElementID);
+    var closeModalButton = document.getElementById(closeModalElementID);
 
     // When the user clicks on the button, open the modal
     openModalButton.addEventListener("click", function(){
-         modal.showModal();
-         customizeOpenModalButton();
+        bodyTags.forEach(body => {
+            body.classList.remove(...body.classList);
+            body.classList.add("stop-scroll");
+        })
+
+        modal.showModal();
     });
     
     closeModalButton.addEventListener("click", () => {
+        bodyTags.forEach(body => {
+            body.classList.remove(...body.classList);
+        })
         modal.close();
     });
+
+    openModalLink.addEventListener('click', () => {
+        bodyTags.forEach(body => {
+            body.classList.remove(...body.classList);
+            body.classList.add("stop-scroll");
+        })
+        modal.showModal();
+    })
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
       if (event.target == modal) {
+          bodyTags.forEach(body => {
+            body.classList.remove(...body.classList);
+        })
           modal.close();
       }
     }
-
-    customizeModalElements();
-})
-
+}
