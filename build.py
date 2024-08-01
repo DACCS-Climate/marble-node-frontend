c087c85c62bba20cdd7fc77ab957cf9ef7ad3f45
alexandercyu <alexanderc.yu@utoronto.ca>
2024-08-01 16:13:24 -0400

    Update build.py
    Add the node registry url to the build

--- build.py
+++ build.py
@@ -15,7 +15,10 @@ def filter_site_templates(template, extensions=("js", "html")):
             "." in basename and
             basename.rsplit(".", 1)[1] in extensions)
 
-def build(build_directory):
+def build(build_directory, node_registry_url, clean=False):
+    if clean:
+        shutil.rmtree(build_directory, ignore_errors=True)
+
     env = Environment(
         loader=FileSystemLoader(TEMPLATE_PATH), autoescape=select_autoescape(enabled_extensions=("html", "js", "css"))
     )
@@ -28,7 +31,7 @@ def build(build_directory):
         )
         os.makedirs(os.path.dirname(build_destination), exist_ok=True)
         with open(build_destination, "w") as f:
-            f.write(env.get_template(template).render())
+            f.write(env.get_template(template).render(node_registry_url=node_registry_url))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -39,6 +42,19 @@ if __name__ == "__main__":
         default=os.path.join(THIS_DIR, "build"),
         help="location on disk to write built templates to.",
     )
+    parser.add_argument(
+        "-u",
+        "--node-registry-url",
+        default="https://raw.githubusercontent.com/"
+                "DACCS-Climate/Marble-node-registry/current-registry/node_registry.json",
+        help="node registry URL",
+    )
+    parser.add_argument(
+        "-c",
+        "--clean",
+        action="store_true",
+        help="clean build directories before building.",
+    )
     args = parser.parse_args()
 
-    build(args.build_directory)
+    build(args.build_directory, args.node_registry_url, args.clean)
