32433afbe61d185c35b94dc5572e09c534e24024
alexandercyu <alexanderc.yu@utoronto.ca>
2024-08-13 17:14:39 -0400

    Update account.js
    More general scripts for the whole Account/Login site

--- templates/site/js/account.js
+++ templates/site/js/account.js
@@ -6,14 +6,22 @@ Get the root url using /session to use with the endpoints in the function below
 const nodeURL = "https://redoak.cs.toronto.edu";
 //const nodeURL = "https://pavics.ouranos.ca";
 
+//TODO Use user credentials (username) to access database (POSTGres) and get their details for Edit page
+
 document.addEventListener("DOMContentLoaded", function () {
-    var signoutButton = document.getElementById("accountLogoutButton");
+    //var signoutButton = document.getElementById("accountLogoutButton");
+    //signoutButton.addEventListener("click",signout);
 
-    signoutButton.addEventListener("click",signout);
+    //var updateButton = document.getElementById("settingsUpdateButton");
+    //signoutButton.addEventListener("click",updateUserDetails);
+
+    //var backButton = document.getElementById("settingsBackButton");
+    //backButton.addEventListener("click", goPreviousPage);
 
 
-    testLogin();
-
+    //testLogin();
+    //var email = getNodeContact();
+    //console.log(email);
     //var userDetailsJSON = getUserDetails();
     //displayAccountDetails(userDetailsJSON);
 
@@ -24,28 +32,32 @@ document.addEventListener("DOMContentLoaded", function () {
 function testLogin(){
 
     const nodeSignIn = nodeURL + "/magpie/signin"
+    //const nodeSignInFragment = "/magpie/signin"
         //const nodeSignIn = nodeURL + "/magpie/ui/login"
-
+    console.log("testLogin");
     fetch(nodeSignIn, {
         method: "POST",
         headers: {
-            Accept: "application/json, text/plain",
+            "Accept": "application/json",
             "Content-Type": "application/json"
             },
         body: {
-            user_name: "alexandercyu",
-            password: ""
+            "user_name": "alexandercyu",
+            "password": "a30nD@CCSUTlive",
+            "provider":"ziggurat"
             }
         }).then(response => response.json()).then(json  => {
-
+ console.log("signin");
+            console.log(json);
         try{
+
             if(json.code == 200){
 
                 //window.location.href = accountHome;
                 //console.log("test login");
 
                 console.log(json.code);
-                console.log(json);
+                //console.log(json);
                 //console.log(user_name);
                 //getSessionDetails();
                 //getUserDetails();
@@ -58,20 +70,31 @@ function testLogin(){
 
         }catch (error){
              //throw new Error(error)
+            console.log("Error caught:")
             console.log(error);
 
         }
     })
 }
 
+function getNodeRegistry() {
+    const githubURL = "{{ node_registry_url }}";
+    return fetch(githubURL).then(response => {
+        return response.json().then(json => {
+            return json;
+        })
+    })
+}
+
 function getSessionDetails(){
     //TODO Remove sessionURL and use sessionURLFragment
+    //TODO Remove nodeURL and use {MAGPIE_URL}
     const sessionURLFragment = "session";
     const sessionURL = nodeURL + "/magpie/session"
     return fetch(sessionURL,{
         method: "GET",
         headers: {
-            Accept: "application/json, text/plain",
+            "Accept": "application/json, text/plain",
             "Content-Type": "application/json"
             }
         }).then(response => { return response.json().then(json =>{
@@ -100,26 +123,31 @@ function getUserDetails(){
     return fetch(loggedUserURL,{
         method: "GET",
         headers: {
-            Accept: "application/json, text/plain",
+            "Accept": "application/json, text/plain",
             "Content-Type": "application/json"
             }
         }).then(response => {return response.json()}).then(json =>{
+            console.log("user details");
+            console.log(json);
             return json;
         })
 }
 
 function getNodeContact(nodeBaseURL){
     const githubURL = "{{ node_registry_url }}";
-    fetch(githubURL).then(response => response.json()).then(json => {
+    return fetch(githubURL).then(response => {return  response.json()}).then(json => {
         var node_keys = Object.keys(json);
         node_keys.forEach((key, index) => {
             if(json[key].links){
                 json[key].links.forEach((link) =>{
                     if(link.rel && link.rel === "service"){
                         if(nodeBaseURL.includes(link.href)){
+                            /*
                             nodeAdminEmail.setAttribute("href", "mailto:" + json[key].contact);
                             passwordResetEmail.setAttribute("href", "mailto:" + json[key].contact);
                             passwordResetEmail.innerText = json[key].contact;
+                            */
+                            return json[key].contact
                         }
                     }
                 })
@@ -127,6 +155,34 @@ function getNodeContact(nodeBaseURL){
         })
     })
 }
+
+//Sets email link on an anchor tag
+function setNodeContact(node_url, elementID){
+    getNodeRegistry().then(json => {
+        var node_keys = Object.keys(json);
+        let node_info;
+
+        node_keys.forEach(key => {
+        node_info = json[key];
+        Object.entries(node_info).forEach(([node_details_key, node_details]) => {
+
+                if(node_details_key == "links"){
+                    node_info[node_details_key].forEach((link) => {
+
+                        if(link.rel == "service"){
+                            if(node_url == link.href){
+                                elementID.href = node_info["contact"];
+                                elementID.innerText = node_info["contact"];
+                            }
+                        }
+                    })
+                }
+            })
+        })
+    });
+}
+
+
 
 function displayAccountDetails(json){
     var h3Header = document.getElementById("h3Header");
@@ -139,6 +195,50 @@ function displayAccountDetails(json){
         accountUsername.innerText = userData["user"].user_name;
         accountEmail.innerText = userData["user"].email;
         dropdownMenuTitle.innerText = userData["user"].user_name;
+    })
+}
+
+
+function updateUserDetails(username, email, password){
+    const updateURLFragment = "users/" + username;
+    const updateURL =  nodeURL + "users/" + username;
+
+    fetch(updateURL, {
+        method: "PATCH",
+        body:{
+            "user_name":username,
+            "email": email,
+            "password": password,
+            "status": 1
+        }
+    })
+}
+
+function updateUserEmail(email){
+    const updateURLFragment = "users/" + username;
+    const updateURL =  nodeURL + "users/" + username;
+
+    fetch(updateURL, {
+        method: "PATCH",
+        body:{
+            "user_name":username,
+            "email": email,
+            "status": 1
+        }
+    })
+}
+
+function updateUserPassword(email){
+    const updateURLFragment = "users/" + username;
+    const updateURL =  nodeURL + "users/" + username;
+
+    fetch(updateURL, {
+        method: "PATCH",
+        body:{
+            "user_name":username,
+            "password": password,
+            "status": 1
+        }
     })
 }
 
@@ -157,4 +257,8 @@ function signout(){
             }
         })
     });
+}
+
+function goPreviousPage(){
+    history.back();
 }
