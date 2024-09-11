2863dc90ef29c92b37fbf481b547c080ee6bd0db
alexandercyu <alexanderc.yu@utoronto.ca>
2024-09-11 16:15:13 -0400

    Create menu.js
    Add scripts to change menu item colour

---
+++ templates/site/js/menu.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var accountButton = document.getElementById('menuItemAccount');
+    var settingsButton = document.getElementById('menuItemSettings');
+    var helpButton = document.getElementById('menuItemHelp');
+
+    var currentURL = window.location.href;
+    var currentPath = window.location.pathname;
+
+    if (currentPath.includes("account.html")) {
+        accountButton.classList.add("nav-item-highlight");
+        settingsButton.classList.remove("nav-item-highlight");
+        helpButton.classList.remove("nav-item-highlight");
+    } else if (currentPath.includes("settings.html")) {
+        accountButton.classList.remove("nav-item-highlight");
+        settingsButton.classList.add("nav-item-highlight");
+        helpButton.classList.remove("nav-item-highlight");
+    } else if (currentPath.includes("help.html")) {
+        accountButton.classList.remove("nav-item-highlight");
+        settingsButton.classList.remove("nav-item-highlight");
+        helpButton.classList.add("nav-item-highlight");
+    } else {
+        accountButton.classList.remove("nav-item-highlight");
+        settingsButton.classList.remove("nav-item-highlight");
+        helpButton.classList.remove("nav-item-highlight");
+    }
+})
