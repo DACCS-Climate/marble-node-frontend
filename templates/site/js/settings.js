81afa80c4fa0a2f44c841bee1a1dcc00d501dcfc
alexandercyu <alexanderc.yu@utoronto.ca>
2024-09-12 13:21:54 -0400

    Update settings.js
    Move setNodeName to main.js

--- templates/site/js/settings.js
+++ templates/site/js/settings.js
@@ -14,31 +14,6 @@ document.addEventListener("DOMContentLoaded", function () {
     saveChangesButton.addEventListener("click",updateUserDetails);
 })
 
-function setNodeName(node_url, elementID){
-
-    getNodeRegistry().then(json => {
-        var node_keys = Object.keys(json);
-        let node_info;
-
-        node_keys.forEach(key => {
-        node_info = json[key];
-        Object.entries(node_info).forEach(([node_details_key, node_details]) => {
-
-                if(node_details_key == "links"){
-                    node_info[node_details_key].forEach((link) => {
-
-                        if(link.rel == "service"){
-                            if(node_url == link.href){
-                                elementID.innerText = node_info["name"];
-                            }
-                        }
-                    })
-                }
-            })
-        })
-    })
-}
-
 function setUserDetails(){
     //TODO Remove placeholder username once design is approved
     var placeholder_username = "XYZ";
