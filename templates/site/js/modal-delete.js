2a6bdf92aac982f8a3923ab7932fb253d70b9d80
alexandercyu <alexanderc.yu@utoronto.ca>
2024-09-12 16:27:15 -0400

    Update modal-delete.js
    Set second delete attention modal for network node popup

--- templates/site/js/modal-delete.js
+++ templates/site/js/modal-delete.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     setModal("deleteAttentionModal", "openDeleteModalButton", "", "closeDeleteModal");
+
+    setModal("deleteNodeAttentionModal", "openDeleteNodePlaceholderID", "", "closeDeleteNodeAttentionModal")
 })
