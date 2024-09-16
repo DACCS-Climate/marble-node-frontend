e5856f4853ed11960760beb0ef0e7f8f069a4084
alexandercyu <alexanderc.yu@utoronto.ca>
2024-09-16 16:43:46 -0400

    Update modal-network-node.js
    Set function to delete a user from a specific node to the delete button in the attention popup called from the network node popup

--- templates/site/js/modal-network-node.js
+++ templates/site/js/modal-network-node.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     setModal("networkNodeModal", "openNetworkModalButton", "openNetworkModalLink", "closeNetworkModal");
+
+    var deleteNodeUserButton = document.getElementById("deleteNodeUserButton");
+    deleteNodeUserButton.addEventListener("click",deleteNodeUser);
+
 })
