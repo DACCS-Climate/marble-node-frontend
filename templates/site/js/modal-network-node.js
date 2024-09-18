b0d497d0295f2150e4db4e3ec3e855fda75ecd69
alexandercyu <alexanderc.yu@utoronto.ca>
2024-09-18 16:08:01 -0400

    Update modal-network-node.js
    Add function to detect scrolling of network popup and add and remove the css styles as necessary

--- templates/site/js/modal-network-node.js
+++ templates/site/js/modal-network-node.js
@@ -4,4 +4,22 @@ document.addEventListener("DOMContentLoaded", function () {
     var deleteNodeUserButton = document.getElementById("deleteNodeUserButton");
     deleteNodeUserButton.addEventListener("click",deleteNodeUser);
 
+    backgroundColourOnScroll();
 })
+
+function backgroundColourOnScroll(){
+    const networkNodeModal = document.getElementById("networkNodeModal");
+    const networkNodeTable = document.querySelector(".popup-network-table");
+    const networkNodeTableHeader = document.querySelector(".popup-network-header");
+
+    networkNodeModal.onscroll = (event) => {
+        if(networkNodeModal.scrollTop > 0){
+            networkNodeTable.classList.add("header-scroll-background");
+            networkNodeTableHeader.classList.remove("border-network-header");
+        }
+        else{
+            networkNodeTable.classList.remove("header-scroll-background");
+            networkNodeTableHeader.classList.add("border-network-header");
+        }
+    }
+}
