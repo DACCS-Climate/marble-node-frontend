28f54fdc7223047b212c1d25e1729907ce611424
alexandercyu <alexanderc.yu@utoronto.ca>
2024-09-24 17:04:33 -0400

    Update modal.js
    - Add conditions for when open/close/text link modal buttons are not available on the page
    - Add condition for when the modal is not available on the page

--- templates/site/js/modal.js
+++ templates/site/js/modal.js
@@ -1,11 +1,25 @@
 function setModal(modalElementID, openModalButtonElementID, openModalLinkElementID, closeModalElementID){
-    // Get the modal
-    var modalElement = document.getElementById(modalElementID);
+    let modalElement;
     var bodyHTMLCollection = document.getElementsByTagName("body");
     var bodyTags = Array.from(bodyHTMLCollection);
 
+    // Get the modal
+    if(modalElementID != "" && document.getElementById(modalElementID) != null){
+        modalElement = document.getElementById(modalElementID);
+
+        // When the user clicks anywhere outside of the modal, close it
+        modalElement.addEventListener("click", () => {
+            if(event.target.id == modalElementID){
+                bodyTags.forEach(body => {
+                    body.classList.remove(...body.classList);
+                })
+                modalElement.close();
+            }
+        } )
+    }
+
     //Adds open modal function to a button
-    if(openModalButtonElementID != ""){
+    if(openModalButtonElementID != "" && document.getElementById(openModalButtonElementID) != null){
         var openModalButton = document.getElementById(openModalButtonElementID);
 
         // When the user clicks on the button, open the modal
@@ -19,7 +33,7 @@ function setModal(modalElementID, openModalButtonElementID, openModalLinkElement
         });
     }
 
-    if(closeModalElementID != ""){
+    if(closeModalElementID != "" && document.getElementById(closeModalElementID) != null){
         var closeModalButton = document.getElementById(closeModalElementID);
 
         closeModalButton.addEventListener("click", () => {
@@ -31,7 +45,7 @@ function setModal(modalElementID, openModalButtonElementID, openModalLinkElement
     }
 
     //Adds open modal function to a text link
-    if(openModalLinkElementID != ""){
+    if(openModalLinkElementID != "" && document.getElementById(openModalLinkElementID) != null){
         var openModalLink = document.getElementById(openModalLinkElementID);
 
         openModalLink.addEventListener('click', () => {
@@ -42,14 +56,4 @@ function setModal(modalElementID, openModalButtonElementID, openModalLinkElement
             modalElement.showModal();
         })
     }
-
-    // When the user clicks anywhere outside of the modal, close it
-    modalElement.addEventListener("click", () => {
-        if(event.target.id == modalElementID){
-            bodyTags.forEach(body => {
-                body.classList.remove(...body.classList);
-            })
-            modalElement.close();
-        }
-    } )
 }
