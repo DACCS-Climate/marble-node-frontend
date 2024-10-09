4aac758d720c988bbc024337d83fe9db11d948a3
alexandercyu <alexanderc.yu@utoronto.ca>
2024-10-09 15:52:25 -0400

    Update help.js
    Remove unneeded functions

--- templates/site/js/help.js
+++ templates/site/js/help.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var sessionDetailsJSON = getSessionDetails();
     var helpContactElement = document.getElementById("helpContact");
 
-    getBaseURL(sessionDetailsJSON).then(baseURL => {
-        setNodeContact(baseURL,helpContactElement);
-    });
+    setNodeContact(helpContactElement);
 })
