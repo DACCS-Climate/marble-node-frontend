c2df49f579c344eba011a50c13f27343ebdc38bd
alexandercyu <alexanderc.yu@utoronto.ca>
2024-10-11 16:05:44 -0400

    Update account.js
    Move displayAccountDetails function here

--- templates/site/js/account.js
+++ templates/site/js/account.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     displayAccountDetails();
 });
+
+function displayAccountDetails(){
+    var h3Header = document.getElementById("h3Header");
+    var accountUsername = document.getElementById("account-username");
+    var accountEmail = document.getElementById("account-email");
+
+    var hiddenUsername = document.getElementById("hiddenUsername");
+    var hiddenEmail = document.getElementById("hiddenEmail");
+
+    console.log(hiddenUsername.value)
+    console.log(hiddenEmail.value)
+    h3Header.innerText = "Hi " + hiddenUsername.value;
+    accountUsername.innerText = hiddenUsername.value;
+    accountEmail.innerText = hiddenEmail.value;
+
+}
