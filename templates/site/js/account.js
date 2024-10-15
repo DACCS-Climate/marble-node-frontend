d449d4a74c3c77286a540883d52975b947a2f5ef
alexandercyu <alexanderc.yu@utoronto.ca>
2024-10-15 18:59:30 -0400

    Update account.js
    - Move display account details function to main.js
    - Change how details are set by using MutationObserver and passing the function to set the user details in the page

--- templates/site/js/account.js
+++ templates/site/js/account.js
@@ -1,19 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    displayAccountDetails();
+    setUserAccountDetails(displayAccountDetails);
 });
-
-function displayAccountDetails(){
-    var h3Header = document.getElementById("h3Header");
-    var accountUsername = document.getElementById("account-username");
-    var accountEmail = document.getElementById("account-email");
-
-    var hiddenUsername = document.getElementById("hiddenUsername");
-    var hiddenEmail = document.getElementById("hiddenEmail");
-
-    console.log(hiddenUsername.value)
-    console.log(hiddenEmail.value)
-    h3Header.innerText = "Hi " + hiddenUsername.innerText;
-    accountUsername.innerText = hiddenUsername.innerText;
-    accountEmail.innerText = hiddenEmail.innerText;
-
-}
