0c3d6c7f2484b5540fc798797567a998ca0b92d8
alexandercyu <alexanderc.yu@utoronto.ca>
2024-10-16 10:43:17 -0400

    Update login.js
    Add ability to login when pressing Enter

--- templates/site/js/login.js
+++ templates/site/js/login.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     var signInButton =  document.getElementById("buttonSignIn");
     var emailButton = document.getElementById("buttonSelectEmail");
+    var userPasswordTextbox = document.getElementById("userPassword");
 
     signInButton.addEventListener('click', (event) => {
         login();
+    })
+
+    userPasswordTextbox.addEventListener('keypress', function (event){
+        if(event.code == 13){
+            login();
+        }
     })
 
     emailButton.addEventListener('click', (event) => {
