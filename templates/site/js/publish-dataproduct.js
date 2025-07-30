805a1f2dbb0bf4f001bef1666b1a5b022da681a9
alexandercyu <alexanderc.yu@utoronto.ca>
2025-07-30 17:03:04 -0400

    Create publish-dataproduct.js
    Move the "change" event function call to a separate file instead of being in the html

---
+++ templates/site/js/publish-dataproduct.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var geometryDropdown = document.getElementById("geometry");
+
+    geometryDropdown.addEventListener("change", geoPolygon2)
+})
