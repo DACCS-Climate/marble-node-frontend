c7215899cf17f9ef2385ecdb28533d382cdb3f14
alexandercyu <alexanderc.yu@utoronto.ca>
2025-07-31 12:30:08 -0400

    Update publish-dataproduct.js
    - added call to functions to clear error message if the focus is on the each of the first author input fields

--- templates/site/js/publish-dataproduct.js
+++ templates/site/js/publish-dataproduct.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     var geometryDropdown = document.getElementById("geometry");
-
     geometryDropdown.addEventListener("change", geoPolygon2)
+
+    var fname1 = document.getElementById("fname1");
+    var lname1 = document.getElementById("lname1");
+    var email1 = document.getElementById("email1");
+
+    fname1.addEventListener("focus", clearAuthorError);
+    lname1.addEventListener("focus", clearAuthorError);
+    email1.addEventListener("focus", clearAuthorError);
+
 })
