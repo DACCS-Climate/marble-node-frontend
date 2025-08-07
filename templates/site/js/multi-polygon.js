afc87bc9ab2e27fd28d03aa3380aa5b019bb482f
alexandercyu <alexanderc.yu@utoronto.ca>
2025-08-07 16:35:23 -0400

    Update multi-polygon.js
    - changed initializeDiv function to add parent divs for all geometry types in the dropdown list so all item input views can be swapped
    - moved creation of geojson upload input field to multi-polygon.js from the publish-dataproduct.html file
    - changed swapDiv function to swap the views of all items in the dropdown list, not just Polygon and MultiPoint
    - addPoint function moved from publish-dataproduct.html to multi-polygon.js
    - commented out addNewPoint function for now
       -> looks like duplicated code

--- templates/site/js/multi-polygon.js
+++ templates/site/js/multi-polygon.js
@@ -2,15 +2,56 @@ function initializeDiv(divID) {
 
     var geoBboxDiv = document.getElementById(divID);
     var geoContentDiv;
+    var geoPointRowsDiv;
+    var geoAddButtonDiv;
+    var addButton;
+    var geojsonUploadDiv;
+    var geojsonUploadInput;
 
+    var geogeojsonUploadTitle = document.createElement("h5");
+    geogeojsonUploadTitle.innerText = "OR: GeoJSON bounding box";
 
     if(divID == "geo_bbox"){
         geoBboxDiv.innerHTML = "";
     }
-
-    if(divID == "geo_multipoint" || divID == "geo_polygon"){
+    else{
         geoBboxDiv.classList.add("show");
         geoContentDiv = document.getElementById(divID + "_content");
+    }
+
+    switch(divID){
+        case "geo_multipoint":
+        case "geo_linestring":
+        case "geo_polygon":
+            geoPointRowsDiv = document.createElement("div");
+            geoPointRowsDiv.id = "geo_multipoint" + "_points_div";
+
+            geoAddButtonDiv = document.createElement("div");
+            geoAddButtonDiv.id = "geo_multipoint" + "_add_button_div";
+
+            geojsonUploadDiv = document.createElement("div");
+            geojsonUploadDiv.id = divID + "_geojson_upload_div";
+
+            geojsonUploadInput = document.createElement("input");
+            geojsonUploadInput.id = divID + "_geojson_file";
+            geojsonUploadInput.setAttribute("type", "text");
+            geojsonUploadInput.setAttribute("name", divID + "File");
+            geojsonUploadInput.classList.add("input-textbox", "margin-input-field");
+
+            addButton = document.createElement("input");
+            addButton.id = divID + "_add_button";
+            addButton.classList.add("button-med", "d-button-text");
+            addButton.setAttribute("type", "button");
+            addButton.setAttribute("value", "Add Point");
+            addButton.addEventListener("click", function(){
+                addPoint(divID + "_content");
+            });
+
+            geoAddButtonDiv.appendChild(addButton);
+            geojsonUploadDiv.appendChild(geogeojsonUploadTitle);
+            geojsonUploadDiv.appendChild(geojsonUploadInput);
+
+            break;
     }
 
 
@@ -36,12 +77,6 @@ function initializeDiv(divID) {
     input2.id = "lon1";
     input2.classList.add("input-textbox", "margin-input-field");
 
-    var addButton = document.createElement("button");
-    addButton.classList.add("addInputButton");
-    addButton.innerText = "Add Point";
-    addButton.addEventListener('click', function (){
-        addNewPoint(divID);
-    });
 
     label1.innerText = "Latitude (Required):";
     label1.setAttribute("for", "bbox1");
@@ -64,41 +99,141 @@ function initializeDiv(divID) {
     coordinateInputContainerDiv.appendChild(latitudeContainer);
     coordinateInputContainerDiv.appendChild(longitudeContainer);
 
-    if(divID == "geo_multipoint" || divID == "geo_polygon"){
-        geoContentDiv.appendChild(coordinateInputContainerDiv);
+
+    switch(divID){
+        case "geo_bbox":
+        case "geo_point":
+            geoContentDiv.appendChild(coordinateInputContainerDiv);
+
+            break;
+
+        case "geo_multipoint":
+        case "geo_linestring":
+        case "geo_polygon":
+            geoPointRowsDiv.appendChild(coordinateInputContainerDiv);
+            geoContentDiv.appendChild(geoPointRowsDiv);
+            geoBboxDiv.appendChild(geoAddButtonDiv);
+            geoBboxDiv.appendChild(geojsonUploadDiv);
+
+            break;
+
+        case "geo_multi_linestring":
+        case "geo_multi_polygon":
+        case "geo_geometry_collection":
+            geoContentDiv.appendChild(coordinateInputContainerDiv);
+
+            break;
     }
-    else{
+
+/*
+    if(divID == "geo_bbox"){
         geoBboxDiv.appendChild(coordinateInputContainerDiv);
     }
+    else{
+        geoPointRowsDiv.appendChild(coordinateInputContainerDiv);
+        geoContentDiv.appendChild(geoPointRowsDiv);
+        geoContentDiv.appendChild(geoAddButtonDiv);
+    }*/
+}
+
+function initializeUploadDiv(divID){
+    var geoContentDiv = document.getElementById(divID + "_content");
+    var uploadTitle = document.createElement("h5");
+    var uploadInput = document.createElement("input");
+    var uploadDiv = document.createElement("div");
+
+    uploadDiv.id = "upload_" + divID;
+    uploadDiv.classList.add("child", "upload-geojson-child");
+
+    uploadTitle.innerText = "GeoJSON bounding box";
+
+    uploadInput.id = "my_" + divID + "_file";
+    uploadInput.setAttribute("type", "text");
+    uploadInput.setAttribute("name", "my_" + divID + "_file");
+    uploadInput.classList.add("input-textbox", "margin-input-field")
+
+    uploadDiv.appendChild(uploadTitle);
+    uploadDiv.appendChild(uploadInput);
+
+    geoContentDiv.appendChild(uploadDiv);
 
 }
+
 
 function swapDiv(divID){
-    var geoMultipointDiv = document.getElementById("geo_multipoint");
-    var geoPolygonDiv = document.getElementById("geo_polygon");
+    var geoBBoxDiv = document.getElementById("geo_bbox");
+    var currentDiv = document.getElementById(divID);
+    var geoNodeList = geoBBoxDiv.querySelectorAll("div.parent");
 
-    if(divID == "geo_multipoint"){
-        if(geoPolygonDiv.classList.contains("show")){
-            geoPolygonDiv.classList.remove("show");
-            geoPolygonDiv.classList.remove("multipoint-parent");
+    for(parentDiv of geoNodeList){
+        if(parentDiv.classList.contains("show")){
+            parentDiv.classList.remove("show");
+            parentDiv.classList.remove("multipoint-parent");
         }
-
-        geoMultipointDiv.classList.add("multipoint-parent");
-        geoMultipointDiv.classList.add("show");
-
-    }
-
-    if(divID == "geo_polygon"){
-        if(geoMultipointDiv.classList.contains("show")){
-            geoMultipointDiv.classList.remove("show");
-            geoMultipointDiv.classList.remove("multipoint-parent");
-        }
-
-        geoPolygonDiv.classList.add("multipoint-parent");
-        geoPolygonDiv.classList.add("show");
-
+        currentDiv.classList.add("multipoint-parent");
+        currentDiv.classList.add("show");
     }
 }
+
+function addPoint(divElementID) {
+        var pointDiv = document.getElementById(divElementID);
+        var autindex = document.querySelectorAll("[id^=lat]").length + 1; // Count existing authors
+
+        var div_box = document.createElement("div");
+        div_box.classList.add("child", "multipoint-additional-child");
+        div_box.id = "multipoint" + autindex;
+
+        var latitudeDiv = document.createElement("div");
+        latitudeDiv.classList.add("latitude-child");
+        var longitudeDiv = document.createElement("div");
+        longitudeDiv.classList.add("longitude-child");
+        var removeButtonDiv = document.createElement("div");
+        removeButtonDiv.classList.add("remove-button");
+
+        var label1 = document.createElement("label");
+        label1.classList.add("subtitle-1");
+        label1.innerText = "Latitude:";
+        label1.setAttribute("for", "lat" + autindex);
+
+        var input1 = document.createElement("input");
+        input1.classList.add("input-textbox", "margin-input-field");
+        input1.setAttribute("type", "text");
+        input1.setAttribute("id", "lat" + autindex);
+        input1.setAttribute("name", "lat[]"); // Make it an array input
+        input1.addEventListener("input", updateAuthorList); // Update list on input
+
+        var label2 = document.createElement("label");
+        label2.classList.add("subtitle-1");
+        label2.innerText = "Longitude:";
+        label2.setAttribute("for", "lon" + autindex);
+
+        var input2 = document.createElement("input");
+        input2.classList.add("input-textbox", "margin-input-field");
+        input2.setAttribute("type", "text");
+        input2.setAttribute("id", "lon" + autindex);
+        input2.setAttribute("name", "lon[]"); // Changed name to array input for last name
+
+        var removePointButton = document.createElement("input");
+        removePointButton.setAttribute("type", "button");
+        removePointButton.value = "Remove Point";
+        removePointButton.classList.add("button-med", "d-button-text");
+        removePointButton.addEventListener("click", function() {
+            removeEntry(divElementID, "multipoint" + autindex);
+        });
+
+        latitudeDiv.appendChild(label1);
+        latitudeDiv.appendChild(input1);
+        longitudeDiv.appendChild(label2);
+        longitudeDiv.appendChild(input2);
+        removeButtonDiv.appendChild(removePointButton);
+
+        div_box.appendChild(latitudeDiv);
+        div_box.appendChild(longitudeDiv);
+        div_box.appendChild(removeButtonDiv);
+
+        pointDiv.appendChild(div_box);
+    }
+
 
 function getDivElements(divID){
 //var divElements = document.getElementById(divID).children
@@ -109,7 +244,7 @@ function getDivElements(divID){
     return arrayLength;
 }
 
-
+/*
 function addNewPoint(divElementID){
     var arrayLength = getDivElements(divElementID);
     var geoBboxDiv = document.getElementById(divElementID);
@@ -146,3 +281,4 @@ function addNewPoint(divElementID){
     geoBboxDiv.appendChild(input2);
 
 }
+*/
