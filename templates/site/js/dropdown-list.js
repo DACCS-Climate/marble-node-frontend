51f9973684818a901dc54bfb4763f05e80cdd043
alexandercyu <alexanderc.yu@utoronto.ca>
2025-08-14 12:20:21 -0400

    Update dropdown-list.js
    Removed swapListItems function

--- templates/site/js/dropdown-list.js
+++ templates/site/js/dropdown-list.js
@@ -1,14 +1,3 @@
-function swapListItems(firstItemID, secondItemID){
-    var firstItem = document.getElementById(firstItemID);
-    var secondItem = document.getElementById(secondItemID);
-
-    var firstItemValue = firstItem.innerHTML;
-
-    firstItem.innerHTML = secondItem.innerHTML;
-    firstItem.setAttribute('swappedID', secondItemID);
-    secondItem.innerHTML = firstItemValue;
-}
-
 function replaceListItem(placeholderValue, firstItemID, secondItemID){
     var firstItem = document.getElementById(firstItemID);
     var secondItem = document.getElementById(secondItemID);
