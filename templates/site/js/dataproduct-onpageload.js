d704f897ee072074710f918630c6943948b62418
alexandercyu <alexanderc.yu@utoronto.ca>
2025-08-29 15:39:11 -0400

    Update dataproduct-onpageload.js
    - add allowInput to flatpickr to allow date validation
    - add change event to Start Date input to copy the date when the copy checkbox is already checked

--- templates/site/js/dataproduct-onpageload.js
+++ templates/site/js/dataproduct-onpageload.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
     /*Initialize flatpickr calendars*/
     flatpickr("#metadata_start_date", {
         enableTime: true,
-        dateFormat: "Y-m-d H:i"
+        dateFormat: "Y-m-d H:i",
+        allowInput: true
     });
     flatpickr("#metadata_end_date", {
         enableTime: true,
-        dateFormat: "Y-m-d H:i"
+        dateFormat: "Y-m-d H:i",
+        allowInput: true
     });
 
     var submitButton = document.getElementById("submit");
@@ -33,7 +35,16 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("metadata_end_date").focus();
     });
 
-    /*For checkbox to make Start Date same as End Date and read only*/
+    /*For Start Date input to check if checkbox to make Start Date same as End Date is checked before copying
+    * Start Date to End Date.
+    * For the case where the checkbox is checked and the Start Date needs to be modified.
+    * */
+    document.getElementById("metadata_start_date").addEventListener("change", () => {
+        copyStartDate("metadata_start_date", "metadata_end_date", "date_make_equal");
+    });
+
+
+    /*For checkbox to make Start Date same as End Date and only End Date read only*/
     document.getElementById("date_make_equal").addEventListener("change", () => {
         calendarDatesEqual("date_make_equal", "date_none_needed", "metadata_start_date", "metadata_end_date");
     });
