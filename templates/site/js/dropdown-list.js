ce9a5cec6f955d2cd04c7c6fddabdcb62e75d962
alexandercyu <alexanderc.yu@utoronto.ca>
2025-11-24 14:52:58 -0500

    Update dropdown-list.js
    Add the selected_offset value to the dropdown label

--- templates/site/js/dropdown-list.js
+++ templates/site/js/dropdown-list.js
@@ -3,10 +3,16 @@ function replaceListItem(titleItemID, selectedItemID){
     var secondItem = document.getElementById(selectedItemID);
     var selectedID;
     var selectedIndex;
+    var selectedOffset;
 
     firstItem.innerHTML = secondItem.innerHTML;
     selectedID = secondItem.id;
     selectedIndex = secondItem.getAttribute("selected_index");
     firstItem.setAttribute("selected_index", selectedIndex);
     firstItem.setAttribute("selected_id", selectedID);
+
+    if(secondItem.hasAttribute("selected_offset")){
+        selectedOffset = secondItem.getAttribute("selected_offset");
+        firstItem.setAttribute("selected_offset", selectedOffset);
+    }
 }
