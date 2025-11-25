87c02444d5bef6bad4554d87c457d5a4a557ccc0
alexandercyu <alexanderc.yu@utoronto.ca>
2025-11-25 16:18:41 -0500

    Update dropdown-list.js
    - add "data-" prefix to custom attributes except for "selected_value"
    - move function for building timezone dropdown list items here from the template

--- templates/site/js/dropdown-list.js
+++ templates/site/js/dropdown-list.js
@@ -7,12 +7,62 @@ function replaceListItem(titleItemID, selectedItemID){
 
     firstItem.innerHTML = secondItem.innerHTML;
     selectedID = secondItem.id;
-    selectedIndex = secondItem.getAttribute("selected_index");
-    firstItem.setAttribute("selected_index", selectedIndex);
+    selectedIndex = secondItem.getAttribute("data-selected_index");
+    firstItem.setAttribute("data-selected_index", selectedIndex);
     firstItem.setAttribute("selected_id", selectedID);
 
-    if(secondItem.hasAttribute("selected_offset")){
-        selectedOffset = secondItem.getAttribute("selected_offset");
-        firstItem.setAttribute("selected_offset", selectedOffset);
+    if(secondItem.hasAttribute("data-selected_offset")){
+        selectedOffset = secondItem.getAttribute("data-selected_offset");
+        firstItem.setAttribute("data-selected_offset", selectedOffset);
     }
+}
+
+function generateTimezoneDropdownListItems(dropdownID, dropdownButtonTextID){
+    var timezoneSet = new Set();
+    var timezoneSetArray;
+    var dropdownULElement = document.getElementById(dropdownID);
+    var generatedTimezonesList = generateTimezoneWithOffset();
+
+    //Make a new list with only UTC and GMT timezone identifiers
+    generatedTimezonesList.forEach( (generatedTimezoneListItem) => {
+       timezoneSet.add(generatedTimezoneListItem.offset);
+    });
+
+    //Sort timezones
+    timezoneSetArray = [...timezoneSet];
+    timezoneSetArray.sort();
+
+    //Put the timezone offset as an attribute in the anchor tag.  Use it to select the list item
+    //when filling out the form with server data.
+    timezoneSetArray.forEach( (timezoneSetArrayItem) => {
+        var timezoneLabel;
+        var timezoneName = timezoneSetArrayItem;
+        var dropdownLIElement = document.createElement("li");
+        var dropdownAnchorElement = document.createElement("a");
+        var dropdownAnchorID = dropdownID + "_" + timezoneSetArrayItem;
+        var timezoneOffsetArray = timezoneSetArrayItem.split("GMT");
+        var timezoneOffset = timezoneOffsetArray[1];
+
+        if(timezoneOffset == "+00:00"){
+            timezoneLabel = "GMT";
+        }
+        else{
+            timezoneLabel = timezoneSetArrayItem;
+        }
+
+        dropdownLIElement.classList.add("dropdown-default-list-item");
+
+        dropdownAnchorElement.classList.add("subtitle-1","dropdown-default-list-item-text");
+        dropdownAnchorElement.setAttribute("role", "button");
+        dropdownAnchorElement.setAttribute("data-selected_index", timezoneName);
+        dropdownAnchorElement.setAttribute("data-selected_offset", timezoneOffset);
+        dropdownAnchorElement.id = dropdownAnchorID;
+        dropdownAnchorElement.innerText = timezoneLabel;
+        dropdownAnchorElement.addEventListener('click', function() {
+            replaceListItem(dropdownButtonTextID, dropdownAnchorID);
+        });
+
+        dropdownLIElement.appendChild(dropdownAnchorElement);
+        dropdownULElement.appendChild(dropdownLIElement);
+    });
 }
